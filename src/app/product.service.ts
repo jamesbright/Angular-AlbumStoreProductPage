1bf2822c13ac181ba038458d5e82b412fd51f5b6
James Ononiwu <jamesononiwu@gmail.com>
2019-10-24 22:42:49 +0100

    part6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
-  private _productsUrl: ' ../assets/products.json';
+  private _productsUrl: string = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,7 +17,7 @@ export class ProductService {
       .map(response => <Album>response.json());
   }
 
-  getProducts(): Observable<Product[]>{
+  getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl)
       .map(response => <Product[]>response.json());
   }
