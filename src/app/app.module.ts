8897d6914ea69bd3af0f7a03d86abc43c322117d
James Ononiwu <jamesononiwu@gmail.com>
2019-10-24 20:30:47 +0100

    part3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import {ProductDescriptionComponent } from './product-description/product-description.component';
-
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import {ProductService } from './product.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import {ProductDescriptionComponent } from './product-description/product-descri
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
